0a22806961445121eb55c9e04b71f460508a1a59
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-07-30 02:43:55 +0900

    Multi process

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import copy
-import threading
+from multiprocessing import Queue, Process
+from typing import NamedTuple, List
 
 import streamlit as st
 from streamlit_webrtc import VideoProcessorBase, webrtc_streamer, WebRtcMode, ClientSettings
@@ -12,25 +13,74 @@ import mediapipe as mp
 from utils import CvFpsCalc
 from main import draw_landmarks, draw_stick_figure
 
+from fake_objects import FakeResultObject, FakeLandmarksObject, FakeLandmarkObject
+
+
+_SENTINEL_ = "_SENTINEL_"
+
+
+def pose_process(
+    in_queue: Queue,
+    out_queue: Queue,
+    static_image_mode,
+    model_complexity,
+    min_detection_confidence,
+    min_tracking_confidence,
+):
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose(
+        static_image_mode=static_image_mode,
+        model_complexity=model_complexity,
+        min_detection_confidence=min_detection_confidence,
+        min_tracking_confidence=min_tracking_confidence,
+    )
+
+    while True:
+        input_item = in_queue.get(timeout=10)
+        if isinstance(input_item, type(_SENTINEL_)) and input_item == _SENTINEL_:
+            break
+
+        results = pose.process(input_item)
+        picklable_results = FakeResultObject(pose_landmarks=FakeLandmarksObject(landmark=[
+            FakeLandmarkObject(
+                x=pose_landmark.x,
+                y=pose_landmark.y,
+                z=pose_landmark.z,
+                visibility=pose_landmark.visibility,
+            ) for pose_landmark in results.pose_landmarks.landmark
+        ]))
+        out_queue.put_nowait(picklable_results)
+
 
 class Tokyo2020PictogramVideoProcessor(VideoProcessorBase):
     def __init__(self, static_image_mode,
                     model_complexity,
                     min_detection_confidence,
                     min_tracking_confidence,rev_color,display_mode) -> None:
-        mp_pose = mp.solutions.pose
-        self._pose = mp_pose.Pose(
-            static_image_mode=static_image_mode,
-            model_complexity=model_complexity,
-            min_detection_confidence=min_detection_confidence,
-            min_tracking_confidence=min_tracking_confidence,
-        )
+        self._in_queue = Queue()
+        self._out_queue = Queue()
+        self._pose_process = Process(target=pose_process, kwargs={
+            "in_queue": self._in_queue,
+            "out_queue": self._out_queue,
+            "static_image_mode": static_image_mode,
+            "model_complexity": model_complexity,
+            "min_detection_confidence": min_detection_confidence,
+            "min_tracking_confidence": min_tracking_confidence,
+        })
         self._cvFpsCalc = CvFpsCalc(buffer_len=10)
 
         self.rev_color = rev_color
         self.display_mode = display_mode
 
-        self._lock = threading.Lock()
+        self._pose_process.start()
+
+    def _infer_pose(self, image):
+        self._in_queue.put_nowait(image)
+        return self._out_queue.get(timeout=10)
+
+    def _stop_pose_process(self):
+        self._in_queue.put_nowait(_SENTINEL_)
+        self._pose_process.join(timeout=10)
 
     def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
         display_fps = self._cvFpsCalc.get()
@@ -56,8 +106,8 @@ class Tokyo2020PictogramVideoProcessor(VideoProcessorBase):
 
         # 検出実施 #############################################################
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-        with self._lock:
-            results = self._pose.process(image)
+        results = self._infer_pose(image)
+        # results = self._pose.process(image)
 
         # 描画 ################################################################
         if results.pose_landmarks is not None:
@@ -92,6 +142,11 @@ class Tokyo2020PictogramVideoProcessor(VideoProcessorBase):
             new_image[offset_y: offset_y + half_h, 0: half_w, :] = cv.resize(debug_image02, (half_w, half_h))
             new_image[offset_y: offset_y + half_h, half_w:, :] = cv.resize(debug_image01, (half_w, half_h))
             return av.VideoFrame.from_ndarray(new_image, format="bgr24")
+
+    def __del__(self):
+        print("Stop the inference process...")
+        self._stop_pose_process()
+        print("Stopped!")
 
 
 def main():
